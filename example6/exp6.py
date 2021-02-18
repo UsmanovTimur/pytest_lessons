c8df30c5ff7190adf366233ebba8cff11f1a7737
Тимур Усманов <usmanov_t@ufanet.ru>
2021-02-18 19:55:21 +0500

    0.0.5

--- example6/exp6.py
+++ example6/exp6.py
@@ -9,4 +9,13 @@ def do_get(url):
     print(resp.json())
 
 
+def do_post(url, data):
+    resp = requests.post(url, json=data)
+    print(resp.json())
+
+
 do_get("https://www.google.com/")
+do_post("https://URL/path/", {
+    "login": "LOGIN",
+    "password": "PASSWORD"
+})
